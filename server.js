e6f1a0a2d71a0754183f9bdcfaa3f1bc67306c2a
tranrate <martin@tranrate.com>
2026-09-13 17:30:15 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+import Fastify from 'fastify'
+
+// Import library to exit fastify process, gracefully (if possible)
+import closeWithGrace from 'close-with-grace'
+
+// Import your application as a normal plugin.
+import appService from './app.js'
+
+// Read the .env file.
+try {
+  process.loadEnvFile()
+} catch {}
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true
+})
+
+// Register your application as a normal plugin.
+app.register(appService)
+
+// delay is the number of milliseconds for the graceful close to finish
+closeWithGrace({ delay: process.env.FASTIFY_CLOSE_GRACE_DELAY || 500 }, async function ({ signal, err, manual }) {
+  if (err) {
+    app.log.error(err)
+  }
+  await app.close()
+})
+
+// Start listening.
+app.listen({ port: process.env.PORT || 3000 }, (err) => {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+})
